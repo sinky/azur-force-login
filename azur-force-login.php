f9b15538730aaa9ee36f0db045bdacb21568630e
Marco Krage <marco@my-azur.de>
2017-05-28 13:49:23 +0200

    added secret link function to share a single post

--- azur-force-login.php
+++ azur-force-login.php
@@ -2,15 +2,35 @@
 /*
 Plugin Name: Azur Force Login
 Plugin URI: http://my-azur.de
-Description: Force Login for everything
-Version: 1.0
+Description: Force Login for everything. Closed/Private Wordpress.
+Version: 2.0
 Author: Marco Krage
 Author URI: http://my-azur.de
 */
 
-function azur_forcelogin() {
-  if ( !is_user_logged_in() ) {
-    auth_redirect();
+/*
+  - In single.php bei the_post_navigation eine 'is_user_logged_in' abfrage um next/prev Beitrag zu verstecken
+  - In content.php bei meta einen "Öffentlichen Link" mit "azur_forcelogin_get_postlink()" hinzugefügt
+*/
+
+function azur_forcelogin_get_postsecret() {
+  $postdata = get_post();
+  return md5($postdata->post_title.$postdata->post_modified);
+}
+
+function azur_forcelogin_get_postlink() {
+  if ( is_user_logged_in() ) {
+    return '<span class="azur-force-login-public-link"><a href="'.get_permalink().'?azurpostsecret='.azur_forcelogin_get_postsecret().'" target="_blank">Öffentlicher Link</a></span>';
   }
+}
+
+function azur_forcelogin() {
+  if(is_single() && $_GET['azurpostsecret'] == azur_forcelogin_get_postsecret()) {
+    $GLOBALS['wp_query']->max_num_pages = 0;
+    return true;
+  }elseif ( is_user_logged_in() ) {
+    return true;
+  }
+  auth_redirect();
 }
 add_action('template_redirect', 'azur_forcelogin');
